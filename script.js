f988aff67ee305f42a7b0d88002304093280ced6
Ele-nonsochi <elea.luzi25@gmail.com>
2021-11-01 21:49:07 +0100

    JS-VUE funzionati

---
+++ script.js
@@ -0,0 +1,50 @@
+Vue.config.devtools = true
+
+const vueApp = new Vue({
+    el: "#root",
+    data: {
+        elements: [
+            {
+                image: 'img/01.jpg',
+                title: "Svezia",
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                image: 'img/02.jpg',
+                title: "Svizzera",
+                text: "testo svizzera",
+            },
+            {
+                image: 'img/03.jpg',
+                title: "Gran Bretagna",
+                text: "testo GB",
+            },
+            {
+                image: 'img/04.jpg',
+                title: "Germania",
+                text: "testo germania",
+            },
+            {
+                image: 'img/05.jpg',
+                title: "Paradise",
+                text: "testo paradise",
+            },
+        ],
+
+        elementCurrent: 0,
+    },
+    methods: {
+        arrowUp() {
+            this.elementCurrent = this.elementCurrent === 0 ? this.elements.length - 1 : this.elementCurrent - 1
+        },
+        arrowBottom() {
+
+            this.elementCurrent = this.elementCurrent === this.elements.length - 1 ? this.elementCurrent = 0 : this.elementCurrent + 1
+        },
+    },
+    mounted() {
+        setInterval(() => {
+            this.arrowBottom()
+        }, 3000);
+    }
+})
